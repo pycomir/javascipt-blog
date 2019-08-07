08326d34ff18ce8840f4dc3038139ae719c23fd9
Mateusz Pycka <matpyc2@gmail.com>
2019-08-07 18:27:57 +0200

    add loop for find selector '.titles a'

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//function to find links <a> in class .titles
+
+/*document.getElementById('test-button').addEventListener('click', function(){
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
+});*/
+
+const titleClickHandler = function(event){
+  console.log(event)
+  console.log('Link was clicked!');
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
